850f3c6ecaf74dec63b5a4321a0134c138c03b93
Bryan Dodd <aqualad@gmail.com>
2016-04-18 10:25:31 -0400

    Add an Avatar component

--- app/index.js
+++ app/index.js
@@ -7,7 +7,6 @@ var USER_DATA = {
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
 /*
     Focused
     Independent
@@ -16,14 +15,45 @@ var HelloWorld = React.createClass({
     Testable
 */
 
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function () {
         return (
-            <div> Hello ReactJS Program! </div>
+            <div>
+                <a href={'https://www.gitub.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         )
     }
 });
 
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+})
+
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
