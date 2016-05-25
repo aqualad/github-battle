0de7257d70a2ea895b86899065e65c39c4b8e114
Bryan Dodd <aqualad@gmail.com>
2016-05-24 23:43:33 -0400

    Removes dummy data

--- app/index.js
+++ app/index.js
@@ -1,9 +1,3 @@
-var USER_DATA = {
-    name: 'Bryan Dodd',
-    username: 'aqualad',
-    image: 'https://avatars0.githubusercontent.com/u/1772354?v=3&s=460'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 
@@ -54,6 +48,5 @@ var Avatar = React.createClass({
 })
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
